7a95f31ad921f2708553aafb89a2ef4bbcee8e58
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-11-17 20:45:21 +0700

    Add kodingan fibonacci.js

--- optional-05-recursive/fibonacci.js
+++ optional-05-recursive/fibonacci.js
@@ -1,5 +1,11 @@
 function fibonacci(n) {
+  // Base case
+  if (n == 0) return [0];
+  if (n == 1) return [0, 1];
 
+  // Recursion
+  const arr = fibonacci(n - 1);
+  return [...arr, arr[n - 1] + arr[n - 2]];
 }
 
 // Jangan hapus kode di bawah ini!
