fb15e2e73ddc5f97ecab1a12b392c76fe2b4b10b
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-11-17 20:44:55 +0700

    Add kodingan factorial.js

--- optional-05-recursive/factorial.js
+++ optional-05-recursive/factorial.js
@@ -1,5 +1,13 @@
 function factorial(n) {
+  // Base case
+  if (n < 0) {
+    throw new Error("Factorial is not defined for negative numbers.");
+  } else if (n === 0 || n === 1) {
+    return 1;
+  }
 
+  // Recursion
+  return n * factorial(n - 1);
 }
 
 // Jangan hapus kode di bawah ini!
