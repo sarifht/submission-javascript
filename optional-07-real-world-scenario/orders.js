d9e04e4000f6318495830ebb3376febc93304d64
Sarif Hidayatullah <hi.sarifhidayatullah@gmail.com>
2024-11-17 20:43:37 +0700

    Update OOP dan order.js pada task ke 7

--- optional-07-real-world-scenario/orders.js
+++ optional-07-real-world-scenario/orders.js
@@ -3,20 +3,45 @@ function generateUniqueId() {
   return `_${Math.random().toString(36).slice(2, 9)}`;
 }
 
-
 // TODO: buatlah variabel yang menampung data orders
-let orders;
+const orders = [];
 
 // TODO: selesaikan fungsi addOrder
-function addOrder(customerName, items) {}
+function addOrder(customerName, items) {
+  const id = generateUniqueId();
+  const order = {
+    id,
+    customerName,
+    items,
+    totalPrice: items.reduce((totalPrice, item) => totalPrice + item.price, 0),
+    status: "Menunggu",
+  };
+  orders.push(order);
+}
 
 // TODO: selesaikan fungsi updateOrderStatus
-function updateOrderStatus(orderId, status) {}
+function updateOrderStatus(orderId, status) {
+  const index = orders.findIndex((order) => order.id === orderId);
+
+  if (index != -1) {
+    orders[index].status = status;
+  }
+}
 
 // TODO: selesaikan fungsi calculateTotalRevenue dari order yang berstatus Selesai
-function calculateTotalRevenue() {}
+function calculateTotalRevenue() {
+  return orders.reduce((totalRevenue, order) => {
+    return order.status == "Selesai" ? totalRevenue + order.totalPrice : totalRevenue;
+  }, 0);
+}
 
 // TODO: selesaikan fungsi deleteOrder
-function deleteOrder(id) {}
+function deleteOrder(id) {
+  const index = orders.findIndex((order) => order.id === id);
+
+  if (index != -1) {
+    orders.splice(index, 1);
+  }
+}
 
 export { orders, addOrder, updateOrderStatus, calculateTotalRevenue, deleteOrder };
